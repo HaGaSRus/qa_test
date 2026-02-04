1c22f0daa5aacad5b24199d3438a294f62050305
George Trofimov <tro37295@gmail.com>
2026-02-04 17:14:01 +0500

    class fill def open and search_address

--- pages/main_page.py
+++ pages/main_page.py
@@ -5,8 +5,20 @@ BASE_URL = "https://piter-online.net/"
 
 
 class MainPage:
-    """Page Object для главной страницы piter-online.net."""
 
     def __init__(self, page: Page) -> None:
         self.page = page
 
+    def open(self) -> None:
+        self.page.goto(BASE_URL)
+
+    def search_address(self, street: str, house: str) -> None:
+        """Ввод адреса в поисковую строку.
+
+        Точные локаторы могут отличаться, поэтому используется
+        максимально устойчивый подход через placeholder и роль.
+        """
+        full_address = f"{street}, {house}"
+        address_input = self.page.get_by_placeholder("Улица и дом").first
+        address_input.fill(full_address)
+        self.page.get_by_role("button", name="Найти").click()
