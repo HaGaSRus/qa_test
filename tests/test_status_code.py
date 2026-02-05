781c241778eb42fd90d50d980f254e1d87479c37
George Trofimov <tro37295@gmail.com>
2026-02-05 09:45:20 +0500

    create test_status_code.py

--- tests/test_status_code.py
+++ tests/test_status_code.py
@@ -2,11 +2,3 @@ import pytest
 from playwright.sync_api import Page
 
 from pages.main_page import BASE_URL
-
-
-@pytest.mark.parametrize("run", range(5))
-def test_main_page_returns_200(page: Page, run: int) -> None:
-    """Проверка, что главная страница отдаёт HTTP 200."""
-    response = page.request.get(BASE_URL)
-    assert response.status == 200
-
