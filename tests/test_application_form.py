acb410ae62b6fbe6614a462a20bf6ea839bfa5bc
George Trofimov <tro37295@gmail.com>
2026-02-05 09:38:49 +0500

    create @pytest.mark.parametrize

--- tests/test_application_form.py
+++ tests/test_application_form.py
@@ -8,3 +8,13 @@ HOUSE = "1"
 NAME = "Автотест"
 PHONE = "1111111111"
 
+
+@pytest.mark.parametrize("run", range(5))
+def test_submit_application_success(page: Page, run: int) -> None:
+    main = MainPage(page)
+    main.open()
+    main.search_address(STREET, HOUSE)
+    main.open_request_form()
+    main.fill_request_form(NAME, PHONE)
+    main.submit_request()
+    main.should_see_success_message()
