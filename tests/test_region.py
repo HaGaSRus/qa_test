42d524445a9ac00e0897ae0bdd3c289909e94f41
George Trofimov <tro37295@gmail.com>
2026-02-05 09:39:54 +0500

    create test_region.py

--- tests/test_region.py
+++ tests/test_region.py
@@ -5,20 +5,3 @@ from pages.main_page import MainPage
 
 
 TARGET_REGION = "Санкт-Петербург"
-
-
-@pytest.mark.parametrize("run", range(5))
-def test_change_region_affects_header(page: Page, run: int) -> None:
-    """Проверка смены региона и отображения выбранного региона в шапке.
-
-    Сценарий:
-    1. Открываем главную страницу.
-    2. Открываем селектор региона.
-    3. Выбираем регион TARGET_REGION.
-    4. Убеждаемся, что выбранный регион отображается в шапке.
-    """
-    main = MainPage(page)
-    main.open()
-    main.change_region(TARGET_REGION)
-    main.current_region_should_be(TARGET_REGION)
-
