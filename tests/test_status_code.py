c8e9457371ae1b8199891452f22b7466b2995566
George Trofimov <tro37295@gmail.com>
2026-02-05 09:46:42 +0500

    create def test_main_page_returns_200

--- tests/test_status_code.py
+++ tests/test_status_code.py
@@ -2,3 +2,9 @@ import pytest
 from playwright.sync_api import Page
 
 from pages.main_page import BASE_URL
+
+
+@pytest.mark.parametrize("run", range(5))
+def test_main_page_returns_200(page: Page, run: int) -> None:
+    response = page.request.get(BASE_URL)
+    assert response.status == 200
