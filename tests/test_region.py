19cd9c9b8e0c9d0d4574c84ba0c99f31a22fb451
George Trofimov <tro37295@gmail.com>
2026-02-05 09:43:46 +0500

    create def test_change_region_affects_header

--- tests/test_region.py
+++ tests/test_region.py
@@ -5,3 +5,11 @@ from pages.main_page import MainPage
 
 
 TARGET_REGION = "Санкт-Петербург"
+
+
+@pytest.mark.parametrize("run", range(5))
+def test_change_region_affects_header(page: Page, run: int) -> None:
+    main = MainPage(page)
+    main.open()
+    main.change_region(TARGET_REGION)
+    main.current_region_should_be(TARGET_REGION)
